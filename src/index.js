c3d20c19f221a1dfedf0cbac7e2a723db2706c01
Dixon McClintock <dixonmcclintock@Dixons-MacBook-Air.local>
2022-11-01 15:10:27 -0400

    add this

--- src/index.js
+++ src/index.js
@@ -3,5 +3,58 @@ import ReactDOM from "react-dom/client";
 import './index.css';
 import ShoppingList from "./ShoppingList";
 
+
+class Game extends React.Component{
+    render(){
+        return(
+            <div className = "game"> 
+            <div>
+                <Board/>
+            </div>
+            </div>
+        );
+    }
+}
+
+class Board extends React.Component{
+   renderSquare(i){
+    return <Square/>
+   }
+   
+    render(){
+        const status = 'X'
+        return(
+            <div className = "Board">
+                <div> Next Player: {status}</div>
+                <div className = "firstRow">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className = "secondRow">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className = "thirdRow">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Square extends React.Component{
+    render(){
+        return(
+            <button className = "square">
+                {""}
+            </button>
+        );
+    }
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<ShoppingList name= "Dixon" item="Child sized firearms"/>);
+root.render(<Game/>);
